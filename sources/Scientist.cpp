5050ef3f14afea7969065a067d7f470563d80e20
KoralElbaz <73183167+KoralElbaz@users.noreply.github.com>
2021-05-18 02:00:26 +0300

    Add files via upload

--- sources/Scientist.cpp
+++ sources/Scientist.cpp
@@ -2,43 +2,43 @@
 using namespace std;
 using namespace pandemic;
 
-Player& Scientist::discover_cure(Color c){
-    if (!board.is_research_stations(this->city))
+Player &Scientist::discover_cure(Color c)
+{
+    if (!board.is_found_cure(c))
     {
-        throw std::invalid_argument{"There is no research station in this city"};
-    }
-
-    int count = 0;
-    for (const auto &t : cards)
-    {
-        if (Board::get_city_color(t) == c)
+        if (!board.is_research_stations(this->city))
         {
-            count++;
+            throw std::invalid_argument{"There is no research station in this city"};
         }
-    }
-    if (count < n)
-    {
-        throw std::invalid_argument{"There are not enough cards"};
-    }
-    if (board.is_found_cure(c))
-    {
-        return *this;
-    }
 
-    int count2=n;
-    auto it = cards.begin();
-    while (it != cards.end() && count2>0)
-    {
+        int count = 0;
+        for (const auto &t : cards)
+        {
+            if (Board::get_city_color(t) == c)
+            {
+                count++;
+            }
+        }
+        if (count < this->n)
+        {
+            throw std::invalid_argument{"There are not enough cards"};
+        }
+
+        int count2 = n;
+        auto it = cards.begin();
+        while (it != cards.end() && count2 > 0)
+        {
             if (Board::get_city_color(*it) == c)
             {
-                it=cards.erase(it);
+                it = cards.erase(it);
                 count2--;
             }
-            else{
+            else
+            {
                 it--;
             }
+        }
+        board.insert_cures(c);
     }
-    board.insert_cures(c);
     return *this;
-
-    };
+};
